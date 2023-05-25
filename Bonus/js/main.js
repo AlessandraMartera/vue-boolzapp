6c1ae9a0a138194f05964e9b067571c62f7a868f
alessandra martera <ale.martera@gmail.com>
2023-05-25 11:52:52 +0200

    Delate message and contact last access

--- Bonus/js/main.js
+++ Bonus/js/main.js
@@ -20,6 +20,7 @@ createApp({
     data() {
       return {
 
+        
         activeChat: 0,
         mainUserName: 'Sofia',
         avatar: './img/avatar_9.png',
@@ -32,16 +33,19 @@ createApp({
                 visible: true,
                 messages: [
                     {
+                        dropDownStatus: false,
                         date: '10/01/2020 15:30:55',
                         message: 'Hai portato a spasso il cane?',
                         status: 'sent'
                     },
                     {
+                        dropDownStatus: false,
                         date: '10/01/2020 15:50:00',
                         message: 'Ricordati di stendere i panni',
                         status: 'sent'
                     },
                     {
+                        dropDownStatus: false,
                         date: '10/01/2020 16:15:22',
                         message: 'Tutto fatto!',
                         status: 'received'
@@ -54,16 +58,20 @@ createApp({
                 visible: true,
                 messages: [
                     {
+                        dropDownStatus: false,
                         date: '20/03/2020 16:30:00',
                         message: 'Ciao come stai?',
                         status: 'sent'
                     },
                     {
+                        dropDownStatus: false,
                         date: '20/03/2020 16:30:55',
                         message: 'Bene grazie! Stasera ci vediamo?',
                         status: 'received'
                     },
                     {
+
+                        dropDownStatus: false,
                         date: '20/03/2020 16:35:00',
                         message: 'Mi piacerebbe ma devo andare a fare la spesa.',
                         status: 'sent'
@@ -76,16 +84,19 @@ createApp({
                 visible: true,
                 messages: [
                     {
+                        dropDownStatus: false,
                         date: '28/03/2020 10:10:40',
                         message: 'La Marianna va in campagna',
                         status: 'received'
                     },
                     {
+                        dropDownStatus: false,
                         date: '28/03/2020 10:20:10',
                         message: 'Sicuro di non aver sbagliato chat?',
                         status: 'sent'
                     },
                     {
+                        dropDownStatus: false,
                         date: '28/03/2020 16:15:22',
                         message: 'Ah scusa!',
                         status: 'received'
@@ -98,11 +109,13 @@ createApp({
                 visible: true,
                 messages: [
                     {
+                        dropDownStatus: false,
                         date: '10/01/2020 15:30:55',
                         message: 'Lo sai che ha aperto una nuova pizzeria?',
                         status: 'sent'
                     },
                     {
+                        dropDownStatus: false,
                         date: '10/01/2020 15:50:00',
                         message: 'Si, ma preferirei andare al cinema',
                         status: 'received'
@@ -115,11 +128,13 @@ createApp({
                 visible: true,
                 messages: [
                     {
+                        dropDownStatus: false,
                         date: '10/01/2020 15:30:55',
                         message: 'Ricordati di chiamare la nonna',
                         status: 'sent'
                     },
                     {
+                        dropDownStatus: false,
                         date: '10/01/2020 15:50:00',
                         message: 'Va bene, stasera la sento',
                         status: 'received'
@@ -132,16 +147,19 @@ createApp({
                 visible: true,
                 messages: [
                     {
+                        dropDownStatus: false,
                         date: '10/01/2020 15:30:55',
                         message: 'Ciao Claudia, hai novità?',
                         status: 'sent'
                     },
                     {
+                        dropDownStatus: false,
                         date: '10/01/2020 15:50:00',
                         message: 'Non ancora',
                         status: 'received'
                     },
                     {
+                        dropDownStatus: false,
                         date: '10/01/2020 15:51:00',
                         message: 'Nessuna nuova, buona nuova',
                         status: 'sent'
@@ -154,11 +172,13 @@ createApp({
                 visible: true,
                 messages: [
                     {
+                        dropDownStatus: false,
                         date: '10/01/2020 15:30:55',
                         message: 'Fai gli auguri a Martina che è il suo compleanno!',
                         status: 'sent'
                     },
                     {
+                        dropDownStatus: false,
                         date: '10/01/2020 15:50:00',
                         message: 'Grazie per avermelo ricordato, le scrivo subito!',
                         status: 'received'
@@ -171,16 +191,19 @@ createApp({
                 visible: true,
                 messages: [
                     {
+                        dropDownStatus: false,
                         date: '10/01/2020 15:30:55',
                         message: 'Ciao, andiamo a mangiare la pizza stasera?',
                         status: 'received'
                     },
                     {
+                        dropDownStatus: false,
                         date: '10/01/2020 15:50:00',
                         message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
                         status: 'sent'
                     },
                     {
+                        dropDownStatus: false,
                         date: '10/01/2020 15:51:00',
                         message: 'OK!!',
                         status: 'received'
@@ -201,18 +224,20 @@ createApp({
         // function to share a Message and an answer 
         shareAMessage() {
             this.contacts[this.activeChat].messages.push({
+                dropDownStatus: false,
                 date: '10/01/2020 15:30:55',
-                        message: this.newTextMessage,
-                        status: 'sent'
+                message: this.newTextMessage,
+                status: 'sent'
             });
 
             this.newTextMessage = '';
 
             setTimeout( () => {
                 this.contacts[this.activeChat].messages.push({
+                    dropDownStatus: false,
                     date: '10/01/2020 15:30:55',
-                            message: 'ok!',
-                            status: 'received'
+                    message: 'ok!',
+                    status: 'received'
                 });
             }, 1000);
         },
@@ -233,8 +258,27 @@ createApp({
            });
             
 
+        },
+
+        activeDropDown (objMessage){
+            console.log(`all'inizio lo status era ${objMessage.dropDownStatus}`);
+            
+            if ( objMessage.dropDownStatus ) {
+                objMessage.dropDownStatus = false;
+            } else {
+                objMessage.dropDownStatus = true;
+            }
+           
+            console.log(`dopo il click lo status e' ${objMessage.dropDownStatus}`);
+            // dropDownStatus: true;
+        },
+        delateMessage(objMessage, i){
+             this.contacts[this.activeChat].messages.splice( i, 1);
         }
 
     }
 
   }).mount('#window_container')
+
+// Cancella messaggio: cliccando sul messaggio appare un menu a tendina che permette di cancellare il messaggio selezionato
+// Visualizzazione ora e ultimo messaggio inviato/ricevuto nella lista dei contatti 
